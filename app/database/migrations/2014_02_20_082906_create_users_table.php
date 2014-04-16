dcbc4b378cf80ef3d9517577249d5da24391286a
Jordi Talens <talensjr@gmail.com>
2014-04-16 13:28:13 +0200

    Sync

--- app/database/migrations/2014_02_20_082906_create_users_table.php
+++ app/database/migrations/2014_02_20_082906_create_users_table.php
@@ -16,6 +16,7 @@ class CreateUsersTable extends Migration {
 			$table->increments('id');
 			$table->string('username');
 			$table->string('password');
+			$table->text('remember_token');
 			$table->timestamps();
 		});
 	}
